07f6ac162b2d2876a635faf25b86645f3d3b760a
Yumi <160249191+eimyat8@users.noreply.github.com>
2025-01-20 20:28:22 +0630

    Update apiResource.js

--- src/api/apiResource.js
+++ src/api/apiResource.js
@@ -9,4 +9,4 @@ export const api = axios.create({
   baseURL: "https://api.openweathermap.org/data/2.5",
 });
 
-export const api_key = "15fe52489f63865ef1a6ed556412e920";
+export const api_key = "";
